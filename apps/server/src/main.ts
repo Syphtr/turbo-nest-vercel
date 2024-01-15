9b7b48414c14adfb1ada2ec6fabe8caffceeb113
TbcProjects <154512269+Recruitpea@users.noreply.github.com>
2024-01-15 17:20:26 +0000

    update port

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -2,10 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const port = process.env.PORT || 4000;
+
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   // const trpc = app.get(TrpcRouter);
   // trpc.applyMiddleware(app);
-  await app.listen(4000);
+  await app.listen(port, '0.0.0.0');
 }
 bootstrap();
