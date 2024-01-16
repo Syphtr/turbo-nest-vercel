105d1ab5e6a761fe08fe5ff5c0d77b4c11c59b9c
TbcProjects <154512269+Recruitpea@users.noreply.github.com>
2024-01-16 11:07:03 +0000

    add another test handler

--- apps/server/src/app.controller.ts
+++ apps/server/src/app.controller.ts
@@ -9,4 +9,9 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('/api/data')
+  getData() {
+    return { message: 'Data from the NestJS API endpoint' };
+  }
 }
