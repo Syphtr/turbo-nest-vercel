215808bcc45852480dbff4a71f9cd8875e85fdc9
TbcProjects <154512269+Recruitpea@users.noreply.github.com>
2024-01-18 09:04:57 +0000

    fix

--- apps/server/src/user/user.controller.ts
+++ apps/server/src/user/user.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { UserService } from './user.service';
-import { User as UserModel } from '@prisma/client';
+// import { User as UserModel } from '@prisma/client';
 
 @Controller('user')
 export class UserController {
@@ -9,7 +9,7 @@ export class UserController {
   @Post('create')
   async signupUser(
     @Body() userData: { name?: string; email: string },
-  ): Promise<UserModel> {
+  ): Promise<any> {
     return this.userService.createUser(userData);
   }
 }
